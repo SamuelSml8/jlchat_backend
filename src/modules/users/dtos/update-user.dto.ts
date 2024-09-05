4fe4ee5ec527589ad17a59893f7b2f53f0e7f99b
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-04 22:17:35 -0500

    add: bio property to update dto

--- src/modules/users/dtos/update-user.dto.ts
+++ src/modules/users/dtos/update-user.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsString, Max } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({ description: 'The bio of the user' })
+  @IsString()
+  @Max(100, { message: 'The bio must have maximum of 100 characters' })
+  bio: string;
+}
