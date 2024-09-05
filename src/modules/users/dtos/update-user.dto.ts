98c345f72b4daf4fb42981700af4f65656ae3efc
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-04 23:06:05 -0500

    refactor: make all fields optional in UpdateUserDto using PartialType

--- src/modules/users/dtos/update-user.dto.ts
+++ src/modules/users/dtos/update-user.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsString, Max } from 'class-validator';
+import { IsOptional, IsString, Max } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @ApiProperty({ description: 'The bio of the user' })
   @IsString()
+  @IsOptional()
   @Max(100, { message: 'The bio must have maximum of 100 characters' })
   bio: string;
 }
