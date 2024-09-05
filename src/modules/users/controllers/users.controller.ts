a3aeb119b34443e0d7082310fa95a228132f357d
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-04 22:04:50 -0500

    feat: endpoint to get a user by email

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dtos';
 
@@ -9,5 +9,10 @@ export class UsersController {
   @Post('create')
   async createUser(@Body() user: CreateUserDto) {
     return await this.usersService.createUser(user);
+  }
+
+  @Get('getByEmail/:email')
+  async getUserByEmail(@Param('email') email: string) {
+    return await this.usersService.getUserByEmail(email);
   }
 }
