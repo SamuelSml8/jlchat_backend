f57d46981fa6c4c03a8614e72d2077c0f5e8155d
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-05 02:17:59 -0500

    refactor: add promises to all endpoints

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -3,23 +3,26 @@ import { AuthService } from '../services/auth.service';
 import { CreateUserDto } from 'src/modules/users/dtos';
 import { LoginDto } from '../dtos/Login.dto';
 import { Token } from 'src/common/types/token.type';
+import { ApiResponse } from 'src/shared/interfaces/response.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() createUserDto: CreateUserDto) {
+  async register(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<ApiResponse<Token>> {
     return this.authService.register(createUserDto);
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto): Promise<ApiResponse<Token>> {
     return this.authService.login(loginDto);
   }
 
   @Post('logout')
-  async logout(@Body() token: Token) {
+  async logout(@Body() token: Token): Promise<ApiResponse<null>> {
     return this.authService.logout(token);
   }
 }
