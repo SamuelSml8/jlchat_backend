7557309af2ca5389f811e47d426e2c3ae2d8411d
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-05 02:14:53 -0500

    feat: added endpoint to logout an user and invalidate a token

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { CreateUserDto } from 'src/modules/users/dtos';
 import { LoginDto } from '../dtos/Login.dto';
+import { Token } from 'src/common/types/token.type';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +16,10 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Post('logout')
+  async logout(@Body() token: Token) {
+    return this.authService.logout(token);
   }
 }
