c4dcf71a3125aca1757f9e9822198494f7516314
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-05 11:22:01 -0500

    feat: users module configured to use guards

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,19 @@ import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { JwtService } from '@nestjs/jwt';
+import { BlackListService } from '../auth/services/black-list.service';
+import { BlackListSchema } from '../auth/schemas/black-list.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'BlackList', schema: BlackListSchema },
+    ]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtService, BlackListService, RolesGuard],
 })
 export class UsersModule {}
