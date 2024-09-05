fcf1489a9b7f9fb07c61c21cb7c313e3f98ede7a
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-05 01:34:20 -0500

    Refactor LoginDto: removed password length validation to ensure flexibility during login, as we're matching against existing stored passwords. Improved error messages for better validation feedback.

--- src/modules/auth/dtos/Login.dto.ts
+++ src/modules/auth/dtos/Login.dto.ts
@@ -8,7 +8,6 @@ export class LoginDto {
   email: string;
 
   @IsNotEmpty({ message: 'Password is required' })
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   @IsString({ message: 'Password must be a string' })
   password: string;
 }
